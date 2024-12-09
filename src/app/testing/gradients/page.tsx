be54909560d105c59feae4d2a4ee4739724e1e66
RohitSth <shrestha.rohit655@gmail.com>
2024-12-09 18:42:00 +0545

    update :: updated the home page

--- src/app/testing/gradients/page.tsx
+++ src/app/testing/gradients/page.tsx
@@ -1,6 +1,6 @@
 export default function Gradients() {
   return (
-    <div className="size-full flex justify-center items-center">
+    <div className="size-full flex justify-center items-center gap-5 h-screen">
       <div className="bg-conic/[in_hsl_longer_hue] from-red-600 to-red-600 size-24 rounded-full"></div>
       <div className="bg-radial-[at_25%_25%] from-white to-zinc-900 to-75% size-24 rounded-full"></div>
     </div>
