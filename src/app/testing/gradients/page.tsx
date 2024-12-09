277e05695a8343e146441dc5d0e186dbd5c60efe
RohitSth <shrestha.rohit655@gmail.com>
2024-12-09 18:30:45 +0545

    update :: testing gradient

---
+++ src/app/testing/gradients/page.tsx
@@ -0,0 +1,8 @@
+export default function Gradients() {
+  return (
+    <div className="size-full flex justify-center items-center">
+      <div className="bg-conic/[in_hsl_longer_hue] from-red-600 to-red-600 size-24 rounded-full"></div>
+      <div className="bg-radial-[at_25%_25%] from-white to-zinc-900 to-75% size-24 rounded-full"></div>
+    </div>
+  );
+}
